5f6dfc03e9a994ceb34a2b4f79f329dd158fcdbc
Kevin Royer <royerkevin@outlook.com>
2022-07-01 15:39:09 -0500

    Week 3 Workshop

--- authenticate.js
+++ authenticate.js
@@ -12,7 +12,7 @@ passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
 exports.getToken = user => {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600})
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 })
 }
 
 const opts = {};
@@ -24,7 +24,7 @@ exports.jwtPassport = passport.use(
         opts,
         (jwt_payload, done) => {
             console.log('JWT payload:', jwt_payload);
-            User.findOne({_id: jwt_payload._id}, (err, user) => {
+            User.findOne({ _id: jwt_payload._id }, (err, user) => {
                 if (err) {
                     return done(err, false);
                 } else if (user) {
@@ -37,4 +37,13 @@ exports.jwtPassport = passport.use(
     )
 );
 
-exports.verifyUser = passport.authenticate('jwt', {session: false});
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        return next()
+    }
+    err = new Error('You are not authorized to perform this operation');
+    err.status = 403;
+    return next(err);
+}
